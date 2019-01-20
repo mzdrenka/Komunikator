54cd4fd8a9bed64ccd537427bb2aa39a7c27b3ee
mzdrenka <zdrenka.mateusz@gmail.com>
2019-01-20 14:14:59 +0100

    Komunikator

--- app/src/main/java/com/bubus/komunikator/CreateMessageActivity.java
+++ app/src/main/java/com/bubus/komunikator/CreateMessageActivity.java
@@ -18,9 +18,18 @@ public class CreateMessageActivity extends Activity {
 
         EditText messageView = (EditText) findViewById(R.id.message);
         String messageText = messageView.getText().toString();
+        String chooserTitle = getString(R.string.chooser);
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, messageText);
+        Intent chosenIntent = Intent.createChooser(intent,chooserTitle);
+     /*
         Intent intent = new Intent(this, ReceiveMessageActivity.class);
         intent.putExtra(ReceiveMessageActivity.EXTRA_MESSAGE, messageText);
-        startActivity(intent);
+     */
+
+
+        startActivity(chosenIntent);
 
     }
 
